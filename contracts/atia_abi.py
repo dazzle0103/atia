3aa5f5dc3adba4f4d6cab7cf0a55e99f8e06d135
dazzle0103 <16864973+dazzle0103@users.noreply.github.com>
2025-05-17 21:46:25 +0200

    Update atia_abi.py
    added getActivationStatus function

--- contracts/atia_abi.py
+++ contracts/atia_abi.py
@@ -1,2 +1,2 @@
 atia_address = '0x9D3936dBd9A794Ee31eF9F13814233D435bD806C'
-atia_abi = '[{"inputs":[{"internalType":"address","name":"to","type":"address"}],"name":"activateStreak","outputs":[],"stateMutability":"nonpayable","type":"function"}]'
+atia_abi = '[{"inputs":[{"internalType":"address","name":"user","type":"address"}],"name":"getActivationStatus","outputs":[{"internalType":"bool","name":"isLostStreak","type":"bool"},{"internalType":"bool","name":"hasPrayedToday","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"}],"name":"activateStreak","outputs":[],"stateMutability":"nonpayable","type":"function"}]'
